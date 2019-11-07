af33699fcf419db2f32849fb52f5c09fd4324d57
almost-friday <31423473+almost-friday@users.noreply.github.com>
2019-11-06 16:27:13 -0800

    Added a play button, difficulty buttons, and options for each play mode
    We just need to make a game now.

--- Project.js
+++ Project.js
@@ -1,19 +1,44 @@
 var gameStyle = false;
+var difficulty = -1;
 
+var PvcOptions = document.getElementById("playerVsComputerOptions");
+var PvpOptions = document.getElementById("playerVsPlayerOptions");
+var PlayButton = document.getElementById("PlayButton");
 
-var PvcOptions = document.getElementById("PvcOptions");
-var PvpOptions = document.getElementById("PvpOptions");
+PvpOptions.style.display = "none";
+PvcOptions.style.display = "none";
+PlayButton.style.display = "none";
 
 function ShowOptions(style) {
-
+    // true for single player, false for 2 players
     gameStyle = style;
     console.debug(gameStyle);
     if (gameStyle == true) {
 
-        options.style.display = "inline-block";
+        PvcOptions.style.display = "inline-block";
+        PvpOptions.style.display = "none";
+    } else {
+        PvpOptions.style.display = "inline-block";
+        PvcOptions.style.display = "none";
     }
+}
+function ChooseDifficulty(difficulty) {
+    if (this.difficulty < 0) { // Only calls when the difficulty hasn't been set!
+        PlayButton.style.opacity = 0;
+        PlayButton.style.display = "inline-block";
+        var opac = 0;
+        var anim = setInterval(ShowPlay, 10);
 
-    else {
-        options.style.display = "none";
     }
+    this.difficulty = difficulty;
+
+    function ShowPlay() {
+        opac += .02;
+        PlayButton.style.opacity = opac;
+        if (opac >= 1) clearInterval(anim);
+   } 
+}
+
+function Play() {
+
 }
