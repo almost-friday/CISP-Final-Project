d2c730ea84d3ce62ea3fe30bb655321b1730295a
almost-friday <31423473+almost-friday@users.noreply.github.com>
2019-11-30 16:18:24 -0800

    Changed location of images, updated JS

--- project.js
+++ project.js
@@ -1,6 +1,10 @@
+// We are defining and initializing several important variables
 var gameStyle = false;
-var difficulty = -1; console.warn("MAKE SURE TO RESET THE DIFFICULTY")
+var difficulty = -1;
+var word = "";
+var guesses = 5;
 
+// The following lines get references to specific elements of the website that we will dynamically show/hide later in the code
 var TitleScreen = document.getElementById("TitleScreen");
 var Game = document.getElementById("Game");
 var Underlines = document.getElementById("Underlines");
@@ -8,13 +12,12 @@ var Guess = document.getElementById("Guess");
 var PvcOptions = document.getElementById("playerVsComputerOptions");
 var PvpOptions = document.getElementById("playerVsPlayerOptions");
 var PlayButton = document.getElementById("PlayButton");
-const WordInput = document.getElementById("WordInput");
+var GuessesLeft = document.getElementById("GuessesLeft");
 
+// These lines hide some elements of the website
 PvpOptions.style.display = "none";
 PvcOptions.style.display = "none";
 PlayButton.style.display = "none";
-Game.style.display = "none";
-WordInput.addEventListener("input", ShowPlay);
 
 
 function ShowOptions(style) {
@@ -31,27 +34,24 @@ function ShowOptions(style) {
     }
 }
 
-function ShowPlay() {
-    WordInput.removeEventListener("input", ShowPlay);
-    PlayButton.style.opacity = 0;
-    PlayButton.style.display = "inline-block";
-    var opac = 0;
-    var anim = setInterval(FadePlay, 10);
+function ChooseDifficulty(difficulty) {
+    if (this.difficulty < 0) { // Only calls when the difficulty hasn't been set!
+        PlayButton.style.opacity = 0;
+        PlayButton.style.display = "inline-block";
+        var opac = 0;
+        var anim = setInterval(ShowPlay, 10);
 
-    function FadePlay() {
+    }
+    this.difficulty = difficulty;
+
+    function ShowPlay() {
         opac += .02;
         PlayButton.style.opacity = opac;
         if (opac >= 1) clearInterval(anim);
-    } 
+   } 
 }
 
-function ChooseDifficulty(difficulty) {
-    if (this.difficulty < 0) { // Only calls when the difficulty hasn't been set!
-        ShowPlay();
-    }
-    this.difficulty = difficulty;
-}
-
+// We are initializing several arrays that hold all of the possible
 function SetupGame() {
 
     var EasyWords = [
@@ -67,55 +67,88 @@ function SetupGame() {
     var HardWords = [
           "cognac", "beignets", "stromboli", "jumbalaya", "enokitake", "acerola", "loquat", "mangosteen", "paneer", "samosas", "fajitas", "tostada", "eucharist", "crabcake"
     ]
-    var word = "";
 
-    switch(difficulty) {
-      case -1:
-        word = WordInput.value;
-        break;
-      case 0:
+
+    if (difficulty == 0) {
         word = EasyWords[getRandomInt(EasyWords.length)];
-        break;
-      case 1:
+    } else if (difficulty == 1) {
         word = MediumWords[getRandomInt(MediumWords.length)];
-        break;
-      case 2:
+    } else {
         word = HardWords[getRandomInt(HardWords.length)];
-        break;
     }
-    console.log(word);
+	word = word.toUpperCase();
+    console.debug(word);
+
 
     TitleScreen.style.display = "none";
     Game.style.display = "inline-block";
 
     for (var i = 0; i < word.length; i ++){
-        showLines();
+        showLines(word.charAt(i));
     }
+	var Lines = document.getElementsByClassName("Line");
+	console.log(Lines[0].id);
+
     for (var i = 65; i <= 90; i++) {
         showLetters(i);
     }
 }
 
-function showLines() {
+
+function showLines(Letter) {
     var block = document.createElement("div");
     var line = document.createElement("img");
     block.appendChild(line);
     line.src = getImageTag();
     line.width = "80";
     line.height = "80";
+	line.className = "Line";
+	line.id = Letter;
     block.style.cssFloat = "left";
     block.style.position = "relative";
     Underlines.appendChild(block);
+
+}
+
+function UpdateLine (char){
+	for (var i = 0; i < word.length; i++){
+
+		if (Lines[i].id == char) {
+		    	var letter = document.createElement("h1");
+			letter.innerHTML = char;
+			Lines[i].img.src = "";
+		}
+	}
 }
 
 function showLetters(keycode) {
     var block = document.createElement("div");
     var letter = document.createElement("h1");
+	var image = document.createElement("img");
+	image.src = "";
     block.appendChild(letter);
     block.className = "Letter"
+	block.onclick = function(){MakeGuess(String.fromCharCode(keycode))}
     letter.innerHTML = String.fromCharCode(keycode);
+	block.appendChild(image);
     Guess.appendChild(block);
+
+function MakeGuess(Letter){
+		console.log(Letter);
+
+	block.onclick = function(){return false;}
+
+	if (word.includes(Letter)){
+		UpdateLine(Letter);
+		letter.innerHTML = "";
+	} else {
+		image.src = "resources/X.png"
+		guesses--;
+		GuessesLeft.innerHTML = "Guesses Left: " + guesses;
+	}
 }
+}
+
 
 function getImageTag() {
     var ImageURLs = [
@@ -128,6 +161,7 @@ function getImageTag() {
     return img;
 }
 
+// A handy function that returns a random number with a parameter that controls the maximum
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
